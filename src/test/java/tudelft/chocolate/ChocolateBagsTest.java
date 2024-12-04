d71e694870212bdd9668cc55016ff074eb9269dd
IuliaaAnd <169478448+IuliiaAnd@users.noreply.github.com>
2024-12-04 12:33:55 -0500

    tests for chocolateBags were already there, deleted them and rewrote myself for practice

--- src/test/java/tudelft/chocolate/ChocolateBagsTest.java
+++ src/test/java/tudelft/chocolate/ChocolateBagsTest.java
@@ -19,19 +19,20 @@ public class ChocolateBagsTest {
         int result = new ChocolateBags().calculate(small, big, total);
         Assertions.assertEquals(expectedResult, result);
     }
-
     @ParameterizedTest(name = "small={0}, big={1}, total={2}, result={3}")
     @CsvSource({
-        "0,3,17,-1", "1,3,17,-1", "2,3,17,2", "3,3,17,2",
-        "0,3,12,-1", "1,3,12,-1", "2,3,12,2", "3,3,12,2"})
-    public void bigAndSmallBars(int small, int big, int total, int expectedResult) {
+            "1,2,11,1", "2,3,16,1", "3,2,13,3", "3,1,8,3", "1,1,12,-1", "-1,-1,2,-1"
+    })
+    public void bigAndSmallBars(int small, int big, int total, int expectedResult){
         int result = new ChocolateBags().calculate(small, big, total);
         Assertions.assertEquals(expectedResult, result);
     }
 
     @ParameterizedTest(name = "small={0}, big={1}, total={2}, result={3}")
-    @CsvSource({ "4,2,3,3", "3,2,3,3", "2,2,3,-1", "1,2,3,-1" })
-    public void onlySmallBars(int small, int big, int total, int expectedResult) {
+    @CsvSource({
+            "1,0,1,1", "2,3,2,2", "3,4,3,3", "4,2,3,3", "1,1,0,0"
+    })
+    public void onlySmallBars(int small, int big, int total, int expectedResult){
         int result = new ChocolateBags().calculate(small, big, total);
         Assertions.assertEquals(expectedResult, result);
     }
