29d1da47db4b99a2cf99ab9273ab2e2de2dc89d3
IuliaaAnd <169478448+IuliiaAnd@users.noreply.github.com>
2024-12-05 17:01:03 -0500

    created gHappy test cases

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,22 @@
 package tudelft.ghappy;
-
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 public class GHappyTest {
+    @ParameterizedTest(name = "{0}: input={1}, expected={2}")
+    @CsvSource({
+            //g is happy
+            "g-happy-middle, xxggxx, true","g-happy-start, ggxxxx, true","g-happy-end, xxxxgg, true",
+            //g not happy
+            "g-not-happy-middle, xxgxx, false", "g-not-happy-start, gxxxx, false","g-not-happy-end, xxxxg, false",
+            //only g string
+            "triple g, ggg, true", "double g, gg, true", "single g, g, false",
+            //g not happy space between
+            "g-not-happy-space, gxgxg, false",
+    })
+    public void gIsHappy(String partition, String str, boolean expectedResult){
+        GHappy happy = new GHappy();
+        boolean result = happy.gHappy(str);
+        Assertions.assertEquals(expectedResult, result);
+    }
 }
