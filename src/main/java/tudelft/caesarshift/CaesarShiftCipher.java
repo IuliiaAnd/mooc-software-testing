6e60e1cd8702aba375b73bc4848c11886aae648f
IuliaaAnd <169478448+IuliiaAnd@users.noreply.github.com>
2024-12-05 18:35:01 -0500

    created CaesarShiftCipher test cases, fixed a bug

--- src/main/java/tudelft/caesarshift/CaesarShiftCipher.java
+++ src/main/java/tudelft/caesarshift/CaesarShiftCipher.java
@@ -2,27 +2,27 @@ package tudelft.caesarshift;
 
 public class CaesarShiftCipher {
 
-    public String CaesarShiftCipher(String message, int shift){
+    public String caesarShiftCipher(String message, int shift){
         StringBuilder sb = new StringBuilder();
         char currentChar;
         int length = message.length();
 
-        shift = shift%26;
+        shift = shift % 26;
 
         for(int i = 0; i < length; i++){
             currentChar = message.charAt(i);
-           
-            sb.append(currentChar);
-            if (currentChar > 'z' || currentChar < 'a') {
-                return "invalid";
-            } else if ((char) (currentChar + shift) > 'z') {
-                currentChar = (char) (currentChar - 26);
-            } else if ((char) (currentChar + shift) < 'a'){
-                currentChar = (char) (currentChar + 26);
-            }
-            sb.append((char) (currentChar + shift));
-        }
 
+            if (currentChar <= 'z' && currentChar >= 'a') {
+                if ((char) (currentChar + shift) > 'z') {
+                    currentChar = (char) (currentChar - 26);
+                } else if ((char) (currentChar + shift) < 'a') {
+                    currentChar = (char) (currentChar + 26);
+                }
+                sb.append((char) (currentChar + shift));
+            } else {
+                return "invalid";
+            }
+        }
         return sb.toString();
     }
 }
--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,25 @@
 package tudelft.caesarshift;
-
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 public class CaesarShiftCipherTest {
-
+    @ParameterizedTest(name = "Message: {0}, Expected: {2} - Shift: {1}")
+    @CsvSource({
+            //valid cases
+            "'abc', 3, 'def'",
+            "'xyz', 3, 'abc'",
+            //valid negative shift
+            "'def', -3, 'abc'",
+            //invalid upper case letters
+            "'AbC', 3, 'invalid'",
+            //invalid spacial characters
+            "'abc!', 4, 'invalid'",
+            //valid big shift
+            "'abc', 29, 'def'"
+    })
+    public void testCaesarCipher(String message, int shift, String expectedResult){
+        CaesarShiftCipher cipher = new CaesarShiftCipher();
+        String result = cipher.caesarShiftCipher(message, shift);
+        Assertions.assertEquals(expectedResult, result);
+    }
 }
